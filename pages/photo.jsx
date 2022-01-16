90b8381a94388ee7af71527a10907347586c955e
Кирилл Павловский <kirill.pavlovskiy@gazprombank.ru>
2022-01-16 19:01:27 +0300

    added carousel

--- pages/photo.jsx
+++ pages/photo.jsx
@@ -1,7 +1,25 @@
 import Head from 'next/head';
 import Image from "next/image";
-import { images } from '../consts/consts';
 import { Carousel } from '@trendyol-js/react-carousel';
+import classes from '@/styles/photo.module.css';
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
+
+const leftArrow = () => {
+    return (
+        <div className={classes.arrow}>
+            <AiOutlineArrowLeft size={30} color='black' className={classes.arrow}/>
+        </div>
+    )
+};
+
+const rightArrow = () => {
+    return (
+        <div className={classes.arrow}>
+            <AiOutlineArrowRight size={30} color='black' className={classes.arrow}/>
+        </div>
+    )
+};
+
 
 const Photo = () => {
     return (
@@ -11,30 +29,59 @@ const Photo = () => {
                 <meta name="keywords" content="блог,павловский,кирилл,видео,саморазвитие"/>
                 <meta name="description" content="блог и видео которые советую посмотреть"/>
             </Head>
-            <div style={{ textAlign: 'center' }}>Страница в стадии разработки</div>
-            {/* <Carousel show={3.5} slide={3} swiping={true}>
-                <Image
-                    src={images[0].src}
-                    width={images[0].width}
-                    height={images[0].height}
-                    alt={images[0].alt}
-                    className={images[0].className}
-                />
-                <Image
-                    src={images[1].src}
-                    width={images[1].width}
-                    height={images[1].height}
-                    alt={images[1].alt}
-                    className={images[1].className}
-                />
-                                <Image
-                    src={images[0].src}
-                    width={images[0].width}
-                    height={images[0].height}
-                    alt={images[0].alt}
-                    className={images[0].className}
-                />
-            </Carousel> */}
+            <div className={classes.wrapper}>
+                <Carousel 
+                    show={3.5}
+                    slide={3}
+                    swiping={true}
+                    leftArrow={leftArrow()}
+                    rightArrow={rightArrow()}
+                    // responsive={true}
+                >
+                    <Image
+                        src='/images/other/carousel/1.jpg'
+                        width={400}
+                        height={400}
+                        alt="1"
+                        className='photo_contain'
+                    />
+                    <Image
+                        src='/images/other/carousel/2.jpg'
+                        width={400}
+                        height={400}
+                        alt="2"
+                        className='photo_contain'
+                    />
+                    <Image
+                        src='/images/other/carousel/3.jpg'
+                        width={400}
+                        height={400}
+                        alt="3"
+                        className='photo_contain'
+                    />
+                    <Image
+                        src='/images/other/carousel/4.jpg'
+                        width={400}
+                        height={400}
+                        alt="4"
+                        className='photo_contain'
+                    />
+                    <Image
+                        src='/images/other/carousel/5.jpg'
+                        width={400}
+                        height={400}
+                        alt="5"
+                        className='photo_contain'
+                    />
+                    <Image
+                        src='/images/other/carousel/6.jpg'
+                        width={400}
+                        height={400}
+                        alt="6"
+                        className='photo_contain'
+                    />
+                </Carousel>
+            </div>
         </>
     )
 };
