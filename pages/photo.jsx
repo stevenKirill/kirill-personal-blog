823984fa8d6c4bb8cf2a0aef44a9fca0661ffec3
Кирилл Павловский <kirill.pavlovskiy@gazprombank.ru>
2022-02-23 14:58:38 +0300

    fixed styled

--- pages/photo.jsx
+++ pages/photo.jsx
@@ -1,27 +1,13 @@
 import Head from 'next/head';
-import Image from "next/image";
-import { Carousel } from '@trendyol-js/react-carousel';
+import { useEffect, useState } from 'react';
+import CarouselComponent from '@/components/Carousel/Carousel';
 import classes from '@/styles/photo.module.css';
-import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
-
-const leftArrow = () => {
-    return (
-        <div className={classes.arrow}>
-            <AiOutlineArrowLeft size={30} color='black' className={classes.arrow}/>
-        </div>
-    )
-};
-
-const rightArrow = () => {
-    return (
-        <div className={classes.arrow}>
-            <AiOutlineArrowRight size={30} color='black' className={classes.arrow}/>
-        </div>
-    )
-};
-
 
 const Photo = () => {
+    const [mounted,setMounted] = useState(false);
+    useEffect(() => {
+        setMounted(true);
+    },[]);
     return (
         <>
             <Head>
@@ -30,57 +16,7 @@ const Photo = () => {
                 <meta name="description" content="блог и видео которые советую посмотреть"/>
             </Head>
             <div className={classes.wrapper}>
-                <Carousel 
-                    show={3.5}
-                    slide={3}
-                    swiping={true}
-                    leftArrow={leftArrow()}
-                    rightArrow={rightArrow()}
-                    responsive={false}
-                >
-                    <Image
-                        src='/images/other/carousel/1.jpg'
-                        width={400}
-                        height={400}
-                        alt="1"
-                        className='photo_contain'
-                    />
-                    <Image
-                        src='/images/other/carousel/2.jpg'
-                        width={400}
-                        height={400}
-                        alt="2"
-                        className='photo_contain'
-                    />
-                    <Image
-                        src='/images/other/carousel/3.jpg'
-                        width={400}
-                        height={400}
-                        alt="3"
-                        className='photo_contain'
-                    />
-                    <Image
-                        src='/images/other/carousel/4.jpg'
-                        width={400}
-                        height={400}
-                        alt="4"
-                        className='photo_contain'
-                    />
-                    <Image
-                        src='/images/other/carousel/5.jpg'
-                        width={400}
-                        height={400}
-                        alt="5"
-                        className='photo_contain'
-                    />
-                    <Image
-                        src='/images/other/carousel/6.jpg'
-                        width={400}
-                        height={400}
-                        alt="6"
-                        className='photo_contain'
-                    />
-                </Carousel>
+                {mounted && <CarouselComponent/>}
             </div>
         </>
     )
