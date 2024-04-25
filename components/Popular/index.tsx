dee735b495382cff13b906831b2f48e5e36ea6c2
Kirill Pavlovskii <kirill-pavlovskij@rambler.ru>
2024-04-25 15:45:52 +0300

    feat: added arrow move

--- components/Popular/index.tsx
+++ components/Popular/index.tsx
@@ -1,30 +1,27 @@
 import React, { FC } from 'react';
 import classes from './classes.module.css';
 import { Blog } from '@/.contentlayer/generated';
-import Link from 'next/link';
-import { FaArrowRight } from 'react-icons/fa';
+import Item from './Item';
 
 interface IProps {
   popularPosts: Blog[];
 }
 
-const Popular: FC<IProps> = ({ popularPosts }) => (
-  <aside className={classes.container}>
-    <h2 className={classes.title}>Популярные</h2>
-    <ul className={classes.postsWrapper}>
-      {popularPosts.map((post) => (
-        <Link
-          href={`/blog/${post.slug}`}
-          className={classes.post}
-          key={post._id}>
-            <span className={classes.icon}>
-              <FaArrowRight color="#617bff" size={16} />
-            </span>
-            <li className={classes.postText}>{post.title}</li>
-        </Link>
-      ))}
-    </ul>
-  </aside>
-);
+const Popular: FC<IProps> = ({ popularPosts }) => {
+
+  return (
+    <aside className={classes.container}>
+      <h2 className={classes.title}>Популярные</h2>
+      <ul className={classes.postsWrapper}>
+        {popularPosts.map((post) => (
+          <Item
+            key={post._id}
+            post={post}
+          />
+        ))}
+      </ul>
+    </aside>
+  );
+}
 
 export default Popular;
