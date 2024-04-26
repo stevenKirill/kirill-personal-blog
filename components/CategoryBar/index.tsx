ba5cb90874e6e7975558cc02e0a16d2c161e3171
Kirill Pavlovskii <kirill-pavlovskij@rambler.ru>
2024-04-26 17:02:13 +0300

    feat: layout responsive design 2

--- components/CategoryBar/index.tsx
+++ components/CategoryBar/index.tsx
@@ -6,25 +6,22 @@ interface ICategoryBarProps {
   categories: string[];
 }
 
-const CategoryBar = ({ categories }: ICategoryBarProps) => {
-  const tmp = [ 'PSYCHOLOGY', 'PROGRAMMING', 'd', 'd', 'f', 'f', 'e', 't' ];
-  return (
-    <aside className={classes.wrapper}>
-      <h2 className={classes.title}>Категории</h2>
-      <div className={classes.linkWrapper}>
-        {tmp.map((category) => {
-          const { text } = getCategoryItem(category);
-          return (
-            <div className={`${classes.link}`} key={text}>
-              <Link href={`/category/${category.toLowerCase()}`}>
-                {text}
-              </Link>
-            </div>
-          );
-        })}
-      </div>
+const CategoryBar = ({ categories }: ICategoryBarProps) => (
+  <aside className={classes.wrapper}>
+    <h2 className={classes.title}>Категории</h2>
+    <div className={classes.linkWrapper}>
+      {categories.map((category) => {
+        const { text } = getCategoryItem(category);
+        return (
+          <div className={`${classes.link}`} key={text}>
+            <Link href={`/category/${category.toLowerCase()}`}>
+              {text}
+            </Link>
+          </div>
+        );
+      })}
+    </div>
   </aside>
-  )
-}
+);
 
 export default CategoryBar;
--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,47 +1,47 @@
 import Link from "next/link";
 import classes from "./footer.module.css";
 
-const Footer = () => {
-  return (
-    <div className={classes.wrapper}>
-      <div className={classes.inner}>
-        <div className={classes.link_wrapper}>
-          <Link href="/" className={classes.link}>
-            <h2>kirill pavlovskii</h2>
-          </Link>
-          <div className={classes.year}>© 2022-present.</div>
-        </div>
-        <div className={classes.right}>
-          <div>
-            <p className={classes.posts}>Статьи</p>
-            <div className={classes.postsLinks}>
-              <a>React</a>
-              <a>JavaScript</a>
-              <a>TypeScript</a>
-              <a>TypeScript</a>
-              <a>TypeScript</a>
-              <a>TypeScript</a>
-            </div>
+// TODO ссылки на статьи в туториалах
+const Footer = () => (
+  <div className={classes.wrapper}>
+    <div className={classes.inner}>
+      <div className={classes.link_wrapper}>
+        <Link href="/" className={classes.link}>
+          <h2>kirill pavlovskii</h2>
+        </Link>
+        <div className={classes.year}>© 2022-present.</div>
+      </div>
+      <div className={classes.right}>
+        <div>
+          <p className={classes.posts}>Статьи</p>
+          <div className={classes.postsLinks}>
+            <a>React</a>
+            <a>JavaScript</a>
+            <a>TypeScript</a>
+            <a>TypeScript</a>
+            <a>TypeScript</a>
+            <a>TypeScript</a>
           </div>
-          <div>
-            <p className={classes.posts}>Ссылки</p>
-            <div className={classes.social}>
-              <a href="https://t.me/kirillPavlovskii" target="_blank" rel="noreferrer">
-                Telegram
-              </a>
-              <a
-                href="https://github.com/stevenKirill"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Github
-              </a>
-            </div>
+        </div>
+        <div>
+          <p className={classes.posts}>Ссылки</p>
+          <div className={classes.social}>
+            <a href="https://t.me/kirillPavlovskii" target="_blank" rel="noreferrer">
+              Telegram
+            </a>
+            <a
+              href="https://github.com/stevenKirill"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
           </div>
         </div>
       </div>
+      <div className={classes.year_responsive}>© 2022-present.</div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Footer;
