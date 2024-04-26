5ce750d53a2744ec61eed8f2beef6e54775105f3
Kirill Pavlovskii <kirill-pavlovskij@rambler.ru>
2024-04-26 14:24:08 +0300

    feat: footer layout

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,21 +1,41 @@
+import Link from "next/link";
 import classes from "./footer.module.css";
-import { FaGithub, FaTelegram } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <div className={classes.wrapper}>
-      <div className={classes.name}>© kirill pavlovskii</div>
-      <div className={classes.icons}>
-        <a href="https://t.me/kirillPavlovskii" target="_blank" rel="noreferrer">
-          <FaTelegram color="white" size="30" className={classes.icon} />
-        </a>
-        <a
-          href="https://github.com/stevenKirill"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaGithub color="white" size="30" className={classes.icon} />
-        </a>
+      <div className={classes.inner}>
+        <div className={classes.link_wrapper}>
+          <Link href="/" className={classes.link}>
+            <h2>kirill pavlovskii</h2>
+          </Link>
+          <div className={classes.year}>© 2022-present.</div>
+        </div>
+        <div className={classes.right}>
+          <div>
+            <p className={classes.posts}>Статьи</p>
+            <div className={classes.postsLinks}>
+              <a>React</a>
+              <a>JavaScript</a>
+              <a>TypeScript</a>
+            </div>
+          </div>
+          <div>
+            <p className={classes.posts}>Ссылки</p>
+            <div className={classes.postsLinks}>
+              <a href="https://t.me/kirillPavlovskii" target="_blank" rel="noreferrer">
+                Telegram
+              </a>
+              <a
+                href="https://github.com/stevenKirill"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Github
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
